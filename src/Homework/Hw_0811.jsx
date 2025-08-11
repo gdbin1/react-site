6a2041a24971a46f61edaa34511bf9b80ca5fa20
gdbin1 <ot5037@naver.com>
2025-08-11 17:32:48 +0900

    17:32

---
+++ src/Homework/Hw_0811.jsx
@@ -0,0 +1,8 @@
+function Hw_0811(){
+    return(
+        <>
+        <h1>테스트</h1>
+        </>
+    )
+}
+export default Hw_0811
--- src/compo/Login.jsx
+++ src/compo/Login.jsx
@@ -1,8 +1,18 @@
-function Login(){
-    return(
+import { useState } from "react"
+function Login() {
+    const [count, setCount] = useState(0)
+    return (
         <div>
             <h2>로그인 페이지 LOGIN</h2>
             {/* 로그인 폼 컴포넌트 추가 */}
+
+
+
+            <h3>useState 이용 숫자 카운터</h3>
+            <h2>카운터: {count}</h2>
+            {/* setCount(count + 1 ) >>> count = count + 1 */}
+            <button onClick={() => setCount(count + 1)}>1씩 ++ 증가</button>
+            <button onClick={() => setCount(count - 1)}>1씩 -- 감소</button>
         </div>
     )
 }
