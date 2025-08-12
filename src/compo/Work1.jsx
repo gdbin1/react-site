d92195297aaece5b9870caa7aecf24a514a7085d
gdbin1 <ot5037@naver.com>
2025-08-12 15:53:56 +0900

    window_width

--- src/compo/Work1.jsx
+++ src/compo/Work1.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 const Work1 = () => {
+    const[width,setWidth] = useState(window.innerWidth)
+
+    useEffect(() =>{
+        const WResize = () => setWidth(window.innerWidth)
+
+        window.addEventListener("resize", WResize)
+        
+        return() => {
+            window.removeEventListener("resize", WResize)
+        }
+
+    }
+    )
     return (
         <div>
-           <h1>8월12일 과제</h1> 
+           <h1>8월12일 과제</h1>
+           <h3>현재 창의 너비: {width}</h3> 
         </div>
     );
 };
