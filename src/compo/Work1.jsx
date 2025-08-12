d733d59812bf6bdb09a90f401805338557ac4629
gdbin1 <ot5037@naver.com>
2025-08-12 17:16:37 +0900

    add color+count

--- src/compo/Work1.jsx
+++ src/compo/Work1.jsx
@@ -6,6 +6,8 @@ const Work1 = () => {
     const [width, setWidth] = useState(window.innerWidth)
     // --------------------
     const[count,setCount] = useState(0)
+    // ----------------------------
+    const[second, setSecond]= useState(0)
 
     useEffect(() => {
         const WResize = () => setWidth(window.innerWidth)
@@ -19,16 +21,29 @@ const Work1 = () => {
     }
     )
     // ---------------
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setSecond(second + 1);
+        }, 1000);
+
+        return() => {
+            clearInterval(timer)
+        }
+    }, [])
+    const[color, setColor] = useState("navy")
     return (
         <div className='gaegu-korean'>
             <h1>8월12일 과제</h1>
             <h3>현재 창의 너비 표시: {width}</h3>
             <hr />
-            <h3>카운트 {count}</h3>
-            <button onClick={() => setCount(count + 1)} >1씩 증가</button>
-            <button onClick={() => setCount(count -1)}>1씩 감소</button>
-            <button onClick={() => setCount(count * 2)}>곱하기 2</button>
-            <button onClick={() => setCount(count / 2)}>나누기 2</button>
+            <h3 style={{color}}>카운트 및 색상 {count}</h3>
+            <button onClick={() => {setCount(count + 1); setColor("red");}} >1씩 증가(빨강)</button>
+            <button onClick={() => {setCount(count -1); setColor("purple");}}>1씩 감소(보라)</button>
+            <button onClick={() => {setCount(count * 2); setColor("yellow");}}>곱하기 2(노랑)</button>
+            <button onClick={() => {setCount(count / 2); setColor("lightgreen");}}>나누기 2(초록)</button>
+            <hr />
+            <h3>{second} 초 경과 </h3>
+            <p>setInterval과 clearInterval은 셋트로 사용, 초를 계속 세고싶으면 []를 지우기</p>
         </div>
     );
 };
